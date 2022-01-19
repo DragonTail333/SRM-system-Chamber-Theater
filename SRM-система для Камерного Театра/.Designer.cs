cefadebe458fbdc68c58470d39c19d7881da1dbc
DragonTail <darkruslan555@gmail.com>
2022-01-20 00:04:15 +0500

    Добавление новой формы
    Здесь просто в этой форме должна высвечиваться информация!

--- SRM-система для Камерного Театра/.Designer.cs
+++ SRM-система для Камерного Театра/.Designer.cs
@@ -32,7 +32,6 @@ namespace SRM_система_для_Камерного_Театра
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -41,6 +40,9 @@ namespace SRM_система_для_Камерного_Театра
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -64,13 +66,6 @@ namespace SRM_система_для_Камерного_Театра
             this.textBox3.Size = new System.Drawing.Size(273, 20);
             this.textBox3.TabIndex = 2;
             // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(107, 159);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(273, 20);
-            this.textBox4.TabIndex = 3;
-            // 
             // textBox5
             // 
             this.textBox5.Location = new System.Drawing.Point(107, 195);
@@ -80,7 +75,7 @@ namespace SRM_система_для_Камерного_Театра
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(107, 233);
+            this.button1.Location = new System.Drawing.Point(107, 267);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(158, 38);
             this.button1.TabIndex = 5;
@@ -110,27 +105,27 @@ namespace SRM_система_для_Камерного_Театра
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(13, 130);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Ответственный";
+            this.label3.Text = "Кампания";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(12, 166);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.Size = new System.Drawing.Size(86, 13);
             this.label4.TabIndex = 9;
-            this.label4.Text = "Телефон";
+            this.label4.Text = "Ответственный";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(13, 202);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.Size = new System.Drawing.Size(52, 13);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Эл. почта";
+            this.label5.Text = "Телефон";
             // 
             // label6
             // 
@@ -141,11 +136,38 @@ namespace SRM_система_для_Камерного_Театра
             this.label6.TabIndex = 11;
             this.label6.Text = "Добавление контакта";
             // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(106, 231);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(273, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Эл. почта";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(107, 158);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(272, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(391, 283);
+            this.ClientSize = new System.Drawing.Size(391, 317);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -154,7 +176,6 @@ namespace SRM_система_для_Камерного_Театра
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -170,7 +191,6 @@ namespace SRM_система_для_Камерного_Театра
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
@@ -179,5 +199,8 @@ namespace SRM_система_для_Камерного_Театра
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
